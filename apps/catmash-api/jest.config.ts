2ddb2fbd22766b8c18c22e50ea8c4ea6548386cb
Ahmed-Zakaria ESDAIRI <ahmedzakariaesdairi@gmail.com>
2024-12-10 22:28:16 +0100

    Initial commit

---
+++ apps/catmash-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'catmash-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/catmash-api',
+};
