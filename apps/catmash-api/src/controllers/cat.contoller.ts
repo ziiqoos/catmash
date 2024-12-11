f5a4d861851e480f2c7e551ce38d21d1e97cb446
Ahmed-Zakaria ESDAIRI <ahmedzakariaesdairi@gmail.com>
2024-12-11 23:21:36 +0100

    feat(API) - Setup backend Architecture and add main routes

---
+++ apps/catmash-api/src/controllers/cat.contoller.ts
@@ -0,0 +1,68 @@
+import { CatService } from '../services/cat.service';
+import { connect } from '../config/db.mongodb';
+import { Request, Response } from 'express';
+export class CatController {
+  private catService: CatService;
+
+  constructor() {
+    connect();
+    this.catService = new CatService();
+  }
+
+
+  async getCatById(req: Request, res: Response): Promise<void> {
+    try {
+      const cat = await this.catService.getCatById(req.params.id);
+      if (!cat) {
+        res.status(404).json({ message: 'Cat not found' });
+      } else {
+        res.status(200).json(cat);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async getAllCats(req: Request, res: Response): Promise<void> {
+    try {
+      const { sort } = req.query;
+      const sortOrder = sort === 'desc' ? -1 : sort === 'asc' ? 1 : undefined;
+
+      const cats = await this.catService.getAllCats(sortOrder);
+      res.status(200).json(cats);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async updateCat(req: Request, res: Response): Promise<void> {
+    try {
+      const updatedCat = await this.catService.updateCat(req.params.id, req.body);
+      if (!updatedCat) {
+        res.status(404).json({ message: 'Cat not found' });
+      } else {
+        res.status(200).json(updatedCat);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async upvoteCat(req: Request, res: Response): Promise<void> {
+    const { catId } = req.params;
+
+    try {
+      const updatedCat = await this.catService.upvoteCat(catId);
+
+      res.status(200).json({ message: "Cat upvoted successfully", data: updatedCat });
+    } catch (error) {
+      if (error.message === "Cat not found") {
+        res.status(404).json({ message: "Cat not found" });
+      } else {
+        console.error("Error upvoting the cat:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+      }
+    }
+
+  }
+}
--- apps/catmash-api/src/main.ts
+++ apps/catmash-api/src/main.ts
@@ -1,19 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
-import * as path from 'path';
+import bodyParser from 'body-parser';
+import catRoutes from './routes/cat.routes';
 import express from 'express';
 
-const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/cats', catRoutes);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to catmash-api!' });
 });
 
+
+// Launch the app on designated port
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
